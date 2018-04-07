6e3c93368c082e84298dab075afb071981d8176b
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-07 14:52:20 -0700

    commit all

--- MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/Program.cs
@@ -10,6 +10,36 @@ namespace MathAndComparisonOperators
     {
         static void Main(string[] args)
         {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int currentTemp = 70;
+            int roomTemp = 69;
+
+            //bool isWarm = currentTemp >= roomTemp;
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
         }
     }
 }
